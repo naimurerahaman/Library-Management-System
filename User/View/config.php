7cf74382b578ca6c1b9fc6ed723a2e94aa27dc85
mosaddek75 <mosaddekmuttakey4390.com>
2025-09-10 18:31:55 +0600

    add config code

---
+++ User/View/config.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "WebTech";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection Fail" . $conn->connect_error);
+}
+
+
+
+?>
